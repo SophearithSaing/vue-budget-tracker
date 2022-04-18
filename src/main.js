d0653f4450d5a02a31fd25e703978f990ebdfe9f
Sophearith Saing <sophearithsaing123@gmail.com>
2022-04-18 23:29:35 +0700

    Add primevue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(PrimeVue)
+  .mount('#app');
