5fc0726b5ce44c456221d4ce3753e58a65d1cd3e
Sophearith Saing <sophearithsaing123@gmail.com>
2022-04-30 23:58:23 +0700

    Add record categories template

--- src/components/AddRecord.vue
+++ src/components/AddRecord.vue
@@ -8,19 +8,22 @@
             <span>Expense</span>
           </template>
           <form class="record__form">
-            <InputNumber
-              id="stacked"
-              v-model="value17"
-              showButtons
-              mode="currency"
-              currency="USD"
-              incrementButtonClass="p-button-danger"
-              decrementButtonClass="p-button-danger"
-            />
-            <Button
-              class="p-button-outlined p-button-danger"
-              icon="pi pi-minus"
-            />
+            <RecordCategories class="record__form--categories" />
+            <div class="record__form--amount">
+              <InputNumber
+                id="stacked"
+                v-model="value17"
+                showButtons
+                mode="currency"
+                currency="USD"
+                incrementButtonClass="p-button-danger"
+                decrementButtonClass="p-button-danger"
+              />
+              <Button
+                class="p-button-outlined p-button-danger"
+                icon="pi pi-minus"
+              />
+            </div>
           </form>
         </TabPanel>
         <TabPanel>
@@ -29,19 +32,22 @@
             <span>Income</span>
           </template>
           <form class="record__form">
-            <InputNumber
-              id="stacked"
-              v-model="value17"
-              showButtons
-              mode="currency"
-              currency="USD"
-              incrementButtonClass="p-button-success"
-              decrementButtonClass="p-button-success"
-            />
-            <Button
-              class="p-button-outlined p-button-success"
-              icon="pi pi-plus"
-            />
+            <RecordCategories class="record__form--categories" />
+            <div class="record__form--amount">
+              <InputNumber
+                id="stacked"
+                v-model="value17"
+                showButtons
+                mode="currency"
+                currency="USD"
+                incrementButtonClass="p-button-success"
+                decrementButtonClass="p-button-success"
+              />
+              <Button
+                class="p-button-outlined p-button-success"
+                icon="pi pi-plus"
+              />
+            </div>
           </form>
         </TabPanel>
       </TabView>
@@ -53,12 +59,14 @@
 import TabView from 'primevue/tabview';
 import TabPanel from 'primevue/tabpanel';
 import InputNumber from 'primevue/inputnumber';
+import RecordCategories from '@/components/RecordCategories.vue';
 
 export default {
   components: {
     TabView,
     TabPanel,
     InputNumber,
+    RecordCategories,
   },
 };
 </script>
@@ -68,7 +76,19 @@ export default {
   &__form {
     display: flex;
     justify-content: center;
-    gap: 10px;
+    @media (max-width: 576px) {
+      flex-direction: column;
+      align-items: center;
+    }
+    gap: 16px;
+    &--categories {
+      width: 200px;
+    }
+    &--amount {
+      display: flex;
+      justify-content: center;
+      gap: 10px;
+    }
   }
 }
 </style>
---
+++ src/components/RecordCategories.vue
@@ -0,0 +1,37 @@
+<template>
+  <TreeSelect
+    v-model="selectedNode"
+    :options="nodes"
+    placeholder="Select Item"
+  />
+</template>
+
+<script>
+import TreeSelect from 'primevue/treeselect';
+
+export default {
+  components: {
+    TreeSelect,
+  },
+  data() {
+    return {
+      nodes: null,
+    };
+  },
+  methods: {
+    getTreeNodes() {
+      return fetch('data/categories.json')
+        .then((res) => res.json())
+        .then((data) => data.categories);
+    },
+  },
+  mounted() {
+    this.getTreeNodes().then((data) => {
+      this.nodes = data;
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
